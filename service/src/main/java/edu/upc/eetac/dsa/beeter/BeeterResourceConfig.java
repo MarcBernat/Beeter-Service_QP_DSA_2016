940b71bc69fe10a406f48fcbfe7690f101c33397
Marc Bernat <marc.bernat@estudiant.upc.edu>
2016-03-02 18:18:17 +0100

    Autorización + POM

--- service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterResourceConfig.java
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterResourceConfig.java
@@ -1,6 +1,7 @@
 package edu.upc.eetac.dsa.beeter;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
 /**
  * Created by marc on 25/02/16.
@@ -8,6 +9,9 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class BeeterResourceConfig extends ResourceConfig {
 
     public BeeterResourceConfig() {
+
         packages("edu.upc.eetac.dsa.beeter");
+        packages("edu.upc.eetac.dsa.beeter.auth");
+        register(RolesAllowedDynamicFeature.class);
     }
 }
