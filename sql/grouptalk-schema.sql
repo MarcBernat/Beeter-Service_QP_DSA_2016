f3e218a3ba4745fe8aa20d5107d3ca7dfcc0dca3
Marc Bernat <marc.bernat@estudiant.upc.edu>
2016-03-03 16:05:23 +0100

    SQL add

---
+++ sql/grouptalk-schema.sql
@@ -0,0 +1,70 @@
+drop database if exists grouptalkdb;
+create database grouptalkdb;
+
+use grouptalkdb;
+
+CREATE TABLE users (
+    id BINARY(16) NOT NULL,
+    loginid VARCHAR(15) NOT NULL UNIQUE,
+    password BINARY(16) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    fullname VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE user_roles (
+    userid BINARY(16) NOT NULL,
+    role ENUM ('registered', 'admin'),
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+    PRIMARY KEY (userid, role)
+);
+
+CREATE TABLE auth_tokens (
+    userid BINARY(16) NOT NULL,
+    token BINARY(16) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+    PRIMARY KEY (token)
+);
+
+CREATE TABLE groups (
+    id BINARY(16) NOT NULL,
+    subject VARCHAR(100) NOT NULL,
+    last_modified TIMESTAMP NOT NULL,
+    creation_timestamp DATETIME not null default current_timestamp,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE group_user_rel (
+    userid BINARY(16) NOT NULL,
+    groupid BINARY(16) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+    FOREIGN KEY (groupid) REFERENCES groups(id) on delete cascade,
+);
+
+CREATE TABLE theme (
+    id BINARY(16) NOT NULL,
+    userid BINARY(16) NOT NULL,
+    subject VARCHAR(100) NOT NULL,
+    last_modified TIMESTAMP NOT NULL,
+    creation_timestamp DATETIME not null default current_timestamp,
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE sting (
+    id BINARY(16) NOT NULL,
+    userid BINARY(16) NOT NULL,
+    content VARCHAR(500) NOT NULL,
+    last_modified TIMESTAMP NOT NULL,
+    creation_timestamp DATETIME not null default current_timestamp,
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE theme_sting_rel (
+    userid BINARY(16) NOT NULL,
+    stingid BINARY(16) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users(id) on delete cascade,
+    FOREIGN KEY (stingid) REFERENCES sting(id) on delete cascade,
+);
+
